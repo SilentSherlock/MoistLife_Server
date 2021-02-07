0d48e6e3e68c46ef0f37b19873366fa6b9c52040
SilentSherlock <1536141515@qq.com>
2021-02-07 17:59:26 +0800

    program init

---
+++ src/main/java/com/program/moist/MoistApplication.java
@@ -0,0 +1,13 @@
+package com.program.moist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/program/moist/MoistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.moist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
